68979aa7587941aa401d0942c8142e0541e2f1a0
Mashrur Ahmed <mashrurahmed500@gmail.com>
2025-09-21 17:52:15 +0600

    aaa

--- Assets/randomCarDrive.cs
+++ Assets/randomCarDrive.cs
@@ -1,18 +1,126 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class randomCarDrive : MonoBehaviour
+public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Inputs
+    private float horizontalInput, verticalInput;
+    private float currentSteerAngle, currentBrakeForce;
+    private bool isBraking;
+
+    // Settings
+    [SerializeField] private float motorForce = 1500f;
+    [SerializeField] private float brakeForce = 3000f;
+    [SerializeField] private float maxSteerAngle = 30f;
+    [SerializeField] private float downforce = 100f;
+
+    // Wheel Colliders
+    [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
+
+    // Wheel Meshes
+    [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
+
+    // Rigidbody reference
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        // Lower the center of mass for stability
+        rb.centerOfMass = new Vector3(0, -0.5f, 0);
+
+        // Freeze tipping (X & Z rotation)
+        //rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        // Adjust wheel grip
+        AdjustFriction(2f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+
+        // Add downforce based on speed
+        rb.AddForce(-transform.up * downforce * rb.velocity.magnitude);
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        isBraking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        // Apply torque to all wheels (AWD)
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        rearLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        rearRightWheelCollider.motorTorque = verticalInput * motorForce;
+
+        // Apply brake force
+        currentBrakeForce = isBraking ? brakeForce : 0f;
+        ApplyBraking();
+    }
+
+    private void ApplyBraking()
+    {
+        frontLeftWheelCollider.brakeTorque = currentBrakeForce;
+        frontRightWheelCollider.brakeTorque = currentBrakeForce;
+        rearLeftWheelCollider.brakeTorque = currentBrakeForce;
+        rearRightWheelCollider.brakeTorque = currentBrakeForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
+    }
+
+    private void AdjustFriction(float stiffness)
+    {
+        WheelFrictionCurve forward = frontLeftWheelCollider.forwardFriction;
+        WheelFrictionCurve sideways = frontLeftWheelCollider.sidewaysFriction;
+
+        forward.stiffness = stiffness;
+        sideways.stiffness = stiffness;
+
+        frontLeftWheelCollider.forwardFriction = forward;
+        frontLeftWheelCollider.sidewaysFriction = sideways;
+
+        frontRightWheelCollider.forwardFriction = forward;
+        frontRightWheelCollider.sidewaysFriction = sideways;
+
+        rearLeftWheelCollider.forwardFriction = forward;
+        rearLeftWheelCollider.sidewaysFriction = sideways;
+
+        rearRightWheelCollider.forwardFriction = forward;
+        rearRightWheelCollider.sidewaysFriction = sideways;
     }
 }
+
