e3c6a0c23e185ee65faccbf270f0b22e1c1e5855
Mashrur Ahmed <mashrurahmed500@gmail.com>
2025-09-21 18:11:41 +0600

    CAR WORKING

--- Assets/randomCarDrive.cs
+++ Assets/randomCarDrive.cs
@@ -1,126 +1,87 @@
-using System;
 using UnityEngine;
 
-public class CarController : MonoBehaviour
+public class SimpleCarController : MonoBehaviour
 {
-    // Inputs
-    private float horizontalInput, verticalInput;
-    private float currentSteerAngle, currentBrakeForce;
+    [Header("Wheel Colliders")]
+    public WheelCollider frontLeftCollider;
+    public WheelCollider frontRightCollider;
+    public WheelCollider rearLeftCollider;
+    public WheelCollider rearRightCollider;
+
+    [Header("Wheel Meshes (Optional for visuals)")]
+    public Transform frontLeftMesh;
+    public Transform frontRightMesh;
+    public Transform rearLeftMesh;
+    public Transform rearRightMesh;
+
+    [Header("Car Settings")]
+    public float motorForce = 1500f;
+    public float brakeForce = 3000f;
+    public float maxSteerAngle = 30f;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steerAngle;
+    private float currentBrakeForce;
     private bool isBraking;
 
-    // Settings
-    [SerializeField] private float motorForce = 1500f;
-    [SerializeField] private float brakeForce = 3000f;
-    [SerializeField] private float maxSteerAngle = 30f;
-    [SerializeField] private float downforce = 100f;
-
-    // Wheel Colliders
-    [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
-    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
-
-    // Wheel Meshes
-    [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
-    [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
-
-    // Rigidbody reference
-    private Rigidbody rb;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-
-        // Lower the center of mass for stability
-        rb.centerOfMass = new Vector3(0, -0.5f, 0);
-
-        // Freeze tipping (X & Z rotation)
-        //rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-
-        // Adjust wheel grip
-        AdjustFriction(2f);
-    }
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         GetInput();
         HandleMotor();
         HandleSteering();
         UpdateWheels();
-
-        // Add downforce based on speed
-        rb.AddForce(-transform.up * downforce * rb.velocity.magnitude);
     }
 
-    private void GetInput()
+    void GetInput()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        isBraking = Input.GetKey(KeyCode.Space);
+        horizontalInput = Input.GetAxis("Horizontal"); // A,D
+        verticalInput = Input.GetAxis("Vertical");   // W,S
+        isBraking = Input.GetKey(KeyCode.Space);     // Space = Brake
     }
 
-    private void HandleMotor()
+    void HandleMotor()
     {
-        // Apply torque to all wheels (AWD)
-        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
-        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
-        rearLeftWheelCollider.motorTorque = verticalInput * motorForce;
-        rearRightWheelCollider.motorTorque = verticalInput * motorForce;
+        // Apply motor torque to rear wheels
+        rearLeftCollider.motorTorque = verticalInput * motorForce;
+        rearRightCollider.motorTorque = verticalInput * motorForce;
 
-        // Apply brake force
+        // Apply braking force
         currentBrakeForce = isBraking ? brakeForce : 0f;
         ApplyBraking();
     }
 
-    private void ApplyBraking()
+    void ApplyBraking()
     {
-        frontLeftWheelCollider.brakeTorque = currentBrakeForce;
-        frontRightWheelCollider.brakeTorque = currentBrakeForce;
-        rearLeftWheelCollider.brakeTorque = currentBrakeForce;
-        rearRightWheelCollider.brakeTorque = currentBrakeForce;
+        frontLeftCollider.brakeTorque = currentBrakeForce;
+        frontRightCollider.brakeTorque = currentBrakeForce;
+        rearLeftCollider.brakeTorque = currentBrakeForce;
+        rearRightCollider.brakeTorque = currentBrakeForce;
     }
 
-    private void HandleSteering()
+    void HandleSteering()
     {
-        currentSteerAngle = maxSteerAngle * horizontalInput;
-        frontLeftWheelCollider.steerAngle = currentSteerAngle;
-        frontRightWheelCollider.steerAngle = currentSteerAngle;
+        steerAngle = maxSteerAngle * horizontalInput;
+        frontLeftCollider.steerAngle = steerAngle;
+        frontRightCollider.steerAngle = steerAngle;
     }
 
-    private void UpdateWheels()
+    void UpdateWheels()
     {
-        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
-        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
-        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
-        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(frontLeftCollider, frontLeftMesh);
+        UpdateSingleWheel(frontRightCollider, frontRightMesh);
+        UpdateSingleWheel(rearLeftCollider, rearLeftMesh);
+        UpdateSingleWheel(rearRightCollider, rearRightMesh);
     }
 
-    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    void UpdateSingleWheel(WheelCollider collider, Transform mesh)
     {
+        if (mesh == null) return;
+
         Vector3 pos;
         Quaternion rot;
-        wheelCollider.GetWorldPose(out pos, out rot);
-        wheelTransform.position = pos;
-        wheelTransform.rotation = rot;
-    }
-
-    private void AdjustFriction(float stiffness)
-    {
-        WheelFrictionCurve forward = frontLeftWheelCollider.forwardFriction;
-        WheelFrictionCurve sideways = frontLeftWheelCollider.sidewaysFriction;
-
-        forward.stiffness = stiffness;
-        sideways.stiffness = stiffness;
-
-        frontLeftWheelCollider.forwardFriction = forward;
-        frontLeftWheelCollider.sidewaysFriction = sideways;
-
-        frontRightWheelCollider.forwardFriction = forward;
-        frontRightWheelCollider.sidewaysFriction = sideways;
-
-        rearLeftWheelCollider.forwardFriction = forward;
-        rearLeftWheelCollider.sidewaysFriction = sideways;
-
-        rearRightWheelCollider.forwardFriction = forward;
-        rearRightWheelCollider.sidewaysFriction = sideways;
+        collider.GetWorldPose(out pos, out rot);
+        mesh.position = pos;
+        mesh.rotation = rot;
     }
 }
-
