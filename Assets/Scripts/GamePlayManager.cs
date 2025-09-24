4fbadca5552400611f07ccd19174fea0f7725b29
Mashrur Ahmed <mashrurahmed500@gmail.com>
2025-09-24 16:06:58 +0600

    as

--- Assets/Scripts/GamePlayManager.cs
+++ Assets/Scripts/GamePlayManager.cs
@@ -1,28 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class GamePlayManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    public static GamePlayManager instance;
-    public GameObject trafficlight;
-    public Material trafficNormalMat, trafficAlertMat;
+    public static GameManager instance;
 
+    [Header("Car Reference")]
+    public AICarController car;   // Drag your car here
+
+    [Header("Wrong Answer Tracking")]
+    private int wrongAnswers = 0;
 
     private void Awake()
     {
-        if (instance == null)
+        // Singleton (so we can access GameManager.instance)
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Called when the player gives a wrong answer
+    /// </summary>
+    public void OnWrongAnswer()
+    {
+        wrongAnswers++;
+
+        if (wrongAnswers == 1)
         {
-            instance = this;
+            MoveCarBack(1);
+        }
+        else if (wrongAnswers == 2)
+        {
+            MoveCarBack(2);
+        }
+        else if (wrongAnswers >= 3)
+        {
+            RespawnAtStart();
+            wrongAnswers = 0; // reset counter
         }
     }
 
-    public void BackToMainMenu(int sceneIndex)
+    private void MoveCarBack(int steps)
     {
-        SceneManager.LoadScene(sceneIndex);
+        if (car != null)
+        {
+            car.MoveBackWaypoints(steps);
+        }
     }
 
-
-
+    private void RespawnAtStart()
+    {
+        if (car != null)
+        {
+            car.RespawnAtStart();
+        }
+    }
 }
+
